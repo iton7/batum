8954bdc5be15aa0a47becab87b0202b50e9813a0
iton7 <71405914+iton7@users.noreply.github.com>
2020-09-16 22:05:43 +0300

    Add files via upload

---
+++ galleria.mobile.js
@@ -0,0 +1,14 @@
+(function($) {
+Galleria.addTheme({
+    name: 'mobile',
+    author: 'Galleria How-to',
+    css: 'galleria.mobile.css',
+    defaults: {
+        transition: 'fade',
+        swipe: true,
+        responsive: true
+    },
+    init: function(options) {
+    }
+});
+}(jQuery));
